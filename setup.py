d744e0f4173836be1be9c1024e677c40d48e9e2e
Kenny Campbell <kennycampbell1@btinternet.com>
2026-07-19 16:50:59 +0100

    Moved debugging print statement

--- setup.py
+++ setup.py
@@ -214,7 +214,6 @@ def get_circuit_filepaths():
 def sort_circ_files_by_type_and_speed(circuit_filepaths) -> [str]:
     """Ad hoc sorting by execution speed (based on my experience of how long they take to run)"""
     sorted_files = sort_files_by_circ_type(circuit_filepaths)
-    print(sorted_files)
     sorted_files = list(sorted_files.values())
     return list(it.chain.from_iterable(sorted_files))
 
